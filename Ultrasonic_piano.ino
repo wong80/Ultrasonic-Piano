5236c9dc5f68353f1695763ef794319d9457fdea
ecyht2 <ecyht2@nottingham.edu.my>
2022-09-08 17:21:35 +0800

    Updated Sound Playing Logic

--- Ultrasonic_piano.ino
+++ Ultrasonic_piano.ino
@@ -12,10 +12,11 @@ int notes[] = {
   NOTE_A3,
   NOTE_B3
 };
-                
+
 char values [] = {'C','D','E','F','G','A','B','N'};
 int trig[] = {41,43,45,47,49,51,53};
 int echo[] = {40,42,44,46,48,50,52};
+int playing = -1;
 //trig pin for ultrasonic sensors {41 - 53}
 //echo pin for ultrasonic sensors {40 - 52}
 
@@ -32,6 +33,7 @@ void setup() {
 }
 
 void loop() {
+  playing = -1;
   for (int i=0; i<7; i+=1){
     digitalWrite(trig[i],LOW);
     delayMicroseconds(2);
@@ -41,69 +43,59 @@ void loop() {
 
     long duration = pulseIn(echo[i],HIGH);
     int distance = 0.0343 * (duration/2);
-    Serial.print("Distance");
+    Serial.print("Distance ");
     Serial.print(i+1);
     Serial.print(" : ");
 
-    Serial.print(distance);
-    Serial.print(" ");
+    Serial.println(distance);
     delayMicroseconds(5);
 
-    if (distance <15 && distance > 0){
-      Serial.println("Sensor");
-      Serial.print(i+1);
-      Serial.print(" will play");
-      Serial.print(values[i]);
-      playTone(values[i]);
-    } else {
-      playTone(values[7]);
+    if (distance < 15 && distance > 0){
+      playing = i;
     }
   }
-  Serial.println("");
+
+  if (playing >= 0) {
+    playTone(values[playing]);
+  } else {
+    playTone(values[7]);
+  }
 }
   
 
 
 void playTone (char values){
+  Serial.println("Playing " + String(values));
   switch (values){
     case 'C':
-      Serial.println("C");
       tone1.play(notes[0]);
       break;
 
     case 'D':
-      Serial.println("D");
       tone1.play(notes[1]);
       break;
 
     case 'E':
-      Serial.println("E");
       tone1.play(notes[2]);
       break;
 
     case 'F':
-      Serial.println("F");
       tone1.play(notes[3]);
       break;
 
     case 'G':
-      Serial.println("G");
       tone1.play(notes[4]);
       break;
 
     case 'A':
-      Serial.println("A");
       tone1.play(notes[5]);
       break;
 
     case 'B':
-      Serial.println("B");
       tone1.play(notes[6]);
       break;
 
     case 'N':
-      Serial.println("N");
       tone1.stop();
   }
-  delay(100);
 }
